b251f661592ae5f84d20764664ee98ad22fc9507
zedatgithub12 <96112790+zedatgithub12@users.noreply.github.com>
2023-04-05 14:52:08 +0300

    not found fixed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/account" element={<CreateAccount />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
 
       </Routes>
 
